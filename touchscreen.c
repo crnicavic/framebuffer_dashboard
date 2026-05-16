217b4f45334550741c51266158da33374b06a752
crnicavic <cavic5000@tutanota.com>
2026-05-16 12:01:18 +0200

    minimal touchscreen example

--- touchscreen.c
+++ touchscreen.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 {
     struct tsdev *ts;
     char *tsdevice = NULL;
-    struct ts_sample_mt **samp_mt = NULL;
+    struct ts_sample samp;
     int ret, i, j;
 
     ts = ts_setup(tsdevice, 0);
@@ -24,50 +24,7 @@ int main(int argc, char **argv)
             return -1;
     }
 
-    samp_mt = malloc(SAMPLES * sizeof(struct ts_sample_mt *));
-    if (!samp_mt) {
-            ts_close(ts);
-            return -ENOMEM;
-    }
-    for (i = 0; i < SAMPLES; i++) {
-            samp_mt[i] = calloc(SLOTS, sizeof(struct ts_sample_mt));
-            if (!samp_mt[i]) {
-                    for (i--; i >= 0; i--)
-                            free(samp_mt[i]);
-                    free(samp_mt);
-                    ts_close(ts);
-                    return -ENOMEM;
-            }
-    }
-
-    while (1) {
-            ret = ts_read_mt(ts, samp_mt, SLOTS, SAMPLES);
-            if (ret < 0) {
-                    perror("ts_read_mt");
-                    ts_close(ts);
-                    exit(1);
-            }
-
-            for (j = 0; j < ret; j++) {
-            	for (i = 0; i < SLOTS; i++) {
-		#ifdef TSLIB_MT_VALID
-			if (!(samp_mt[j][i].valid & TSLIB_MT_VALID))
-				continue;
-		#else
-			if (samp_mt[j][i].valid < 1)
-				continue;
-		#endif
-
-			printf("%lld.%06lld: (slot %d) %6d %6d %6d\n",
-			       (long long)samp_mt[j][i].tv.tv_sec,
-			       (long long)samp_mt[j][i].tv.tv_usec,
-			       samp_mt[j][i].slot,
-			       samp_mt[j][i].x,
-			       samp_mt[j][i].y,
-			       samp_mt[j][i].pressure);
-                    }
-            }
-    }
-
+    ts_read(ts, &samp, 1);
+    printf("%d, %d", samp.x, samp.y);
     ts_close(ts);
 }
